d67d63f0a52636b2d15abc23c39d2808234a8d74
LuKane <1169604556@qq.com>
2020-12-04 16:08:23 +0800

    Date is finished

--- Mamba/Mamba/Kind/Date+Mamba.swift
+++ Mamba/Mamba/Kind/Date+Mamba.swift
@@ -73,6 +73,112 @@ extension Date {
         return dateFormat.string(from: self)
     }
     
+    /*****************************************************************************/
     
+    /// is the same year compare to otherDate
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same year
+    func isSameYear(otherDate: Date) -> Bool {
+        let now   = Calendar.current.dateComponents([ .era, .year], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year], from: otherDate)
+        if now.era == other.era && now.year == other.year {
+            return true
+        }
+        return false
+    }
     
+    /// is the same month compare to otherDate [but first is need to compare Year]
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same month
+    func isSameMonth(otherDate: Date) -> Bool {
+        if isSameYear(otherDate: otherDate) == false {
+            return false
+        }
+        
+        let now   = Calendar.current.dateComponents([ .era, .year, .month], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year, .month], from: otherDate)
+        if now.era == other.era && now.year == other.year && now.month == other.month {
+            return true
+        }
+        return false
+    }
+    
+    /// is the same day compare to otherDate [but first is need to compare Year && Month]
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same day
+    func isSameDay(otherDate: Date) -> Bool {
+        if isSameMonth(otherDate: otherDate) == false {
+            return false
+        }
+        
+        let now   = Calendar.current.dateComponents([ .era, .year, .month, .day], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year, .month, .day], from: otherDate)
+        if now.era == other.era &&
+            now.year == other.year &&
+            now.month == other.month &&
+            now.day == other.day{
+            return true
+        }
+        return false
+    }
+    
+    /// is the same hour compare to otherDate [but first is need to compare Year && Month && Day]
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same hour
+    func isSameHour(otherDate: Date) -> Bool {
+        if isSameDay(otherDate: otherDate) == false {
+            return false
+        }
+        let now   = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour], from: otherDate)
+        if now.era == other.era &&
+            now.year == other.year &&
+            now.month == other.month &&
+            now.day == other.day &&
+            now.hour == other.hour {
+            return true
+        }
+        return false
+    }
+    
+    /// is the same min compare to otherDate [but first is need to compare Year && Month && Day && Hour]
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same min
+    func isSameMin(otherDate: Date) -> Bool {
+        if isSameHour(otherDate: otherDate) == false {
+            return false
+        }
+        let now   = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour, .minute], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour, .minute], from: otherDate)
+        if now.era == other.era &&
+            now.year == other.year &&
+            now.month == other.month &&
+            now.day == other.day &&
+            now.hour == other.hour &&
+            now.minute == other.minute {
+            return true
+        }
+        return false
+    }
+    
+    /// is the same second compare to otherDate [but first is need to compare Year && Month && Day && Hour && Min]
+    /// - Parameter otherDate: otherDate
+    /// - Returns: is the same second
+    func isSameSecond(otherDate: Date) -> Bool {
+        if isSameMin(otherDate: otherDate) == false {
+            return false
+        }
+        let now   = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour, .minute, .second], from: self)
+        let other = Calendar.current.dateComponents([ .era, .year, .month, .day, .hour, .minute, .second], from: otherDate)
+        if now.era == other.era &&
+            now.year == other.year &&
+            now.month == other.month &&
+            now.day == other.day &&
+            now.hour == other.hour &&
+            now.minute == other.minute &&
+            now.second == other.second {
+            return true
+        }
+        return false
+    }
 }
