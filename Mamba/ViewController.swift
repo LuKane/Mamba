d6df409672cb5d31017f6d980087888f2002ba70
LuKane <1169604556@qq.com>
2020-12-06 21:39:28 +0800

    test code

--- Mamba/ViewController.swift
+++ Mamba/ViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        
+        let time = "2018-12-28 19:44:33"
+        let date = Date.dateFromString(time, format: "YYYY-MM-dd HH:mm:ss")
+        
+        print("\(String(describing: date?.dateAddingDays(days: 3)))")
     }
 }
