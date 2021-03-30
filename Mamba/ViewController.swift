f97ec51706b93aa433cef81fe777f040be49ac4d
LuKane <1169604556@qq.com>
2021-03-30 17:11:57 +0800

    name change

--- Mamba/ViewController.swift
+++ Mamba/ViewController.swift
@@ -7,15 +7,39 @@
 
 import UIKit
 
+class Object: NSObject {
+    var name: String = ""
+    var age: String = ""
+}
+
 class ViewController: UIViewController {
-    
-    var name: String = "Jack"
-    var name1: String = "Rose"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         automaticallyAdjustsScrollViewInsets = false
         edgesForExtendedLayout = UIRectEdge.init(rawValue: 0)
         view.backgroundColor = .orange
+        
+        let objc = Object()
+        objc.name = "1"
+        
+        print("\(String(describing: JudgeEmpty.isEmptyString(str: objc.name)))")
+        
     }
 }
+
+class JudgeEmpty {
+    class func isEmptyString(str: String?) -> Bool {
+        guard str != nil else {
+            return true
+        }
+        return str?.isEmpty ?? false
+    }
+    class func isEmptyArray(arr: [Any]?) -> Bool {
+        guard arr != nil else {
+            return true
+        }
+        return arr?.isEmpty ?? false
+    }
+}
+
